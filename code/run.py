853e82a5e715a4106248118c0cf10f447dfdce48
BWorkman09 <bwworkma@uark.edu>
2025-03-31 14:58:21 -0500

    Update run.py

--- code/run.py
+++ code/run.py
@@ -5,7 +5,7 @@ import yaml
 from api.routes import api_bp
 from pathlib import Path
 from flask_sqlalchemy import SQLAlchemy
-from api.models import db, User, Article, Category, UserPreference
+
 
 # Using Blueprints to organize routes in a Flask application
 # https://flask.palletsprojects.com/en/2.0.x/blueprints/
@@ -17,28 +17,6 @@ from api.models import db, User, Article, Category, UserPreference
 #  prefix of "/api", which means that all routes defined in the blueprint will
 #  be prefixed with "/api".  For example, a route defined in the blueprint as
 #  "/users" will be accessible at "/api/users" in the application.
-
-
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-
-    # If you have provided an openapi.yaml file in the docs folder, load it
-    # This will allow you to use Swagger UI to view and test your API endpoints
-    #  Run the app and go to http://localhost:5000/apidocs to view the Swagger UI
-    # Load OpenAPI specification from YAML file
-    if Path.exists(Path("docs/openapi.yaml")):
-        with open("docs/openapi.yaml", "r") as file:
-            openapi_spec = yaml.safe_load(file)
-
-        # Initialize Swagger with the OpenAPI spec
-        swagger = Swagger(app, template=openapi_spec)
-
-    # Register Blueprints
-    app.register_blueprint(api_bp, url_prefix="/api")
-
-    return app
-
 
 
 def create_app():
